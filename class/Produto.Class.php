e570183b4c3450602a8c287d216270fe3e71c2c3
AndrssPessoa <103079879+AndrssBarros@users.noreply.github.com>
2023-05-04 21:37:46 -0300

    Update Produto.Class.php

--- class/Produto.Class.php
+++ class/Produto.Class.php
@@ -59,8 +59,42 @@ include_once("class/DB.Class.php");
             return $this->quantidade;
         }
 
-        public function adicionar(){} 
-        public function listar(){} 
+        public function adicionar(){
+           try {$sql = "INSERT INTO produtos(categoria_id,nome,preco,quantidade)
+                    VALUES(?,?,?,?)";
+
+                    $conexao = DB::conexao();
+                    $stmt    = $conexao->prepare($sql);
+                    $stmt->bindParam(1,$this->categoria_id);
+                    $stmt->bindParam(2,$this->nome);
+                    $stmt->bindParam(3,$this->preco);
+                    $stmt->bindParam(4,$this->quantidade);
+                    $stmt->execute();
+                    }catch(PDOException $e){
+                        echo "Erro na Função adicionar Produto" . $e->getMessage();
+                    }
+                } 
+        public static function listar(){
+            $sql     = "SELECT * FROM produtos";
+            $conexao = DB::conexao();
+            $stmt    = $conexao->prepare($sql);
+            $stmt->execute();
+            $registros= $stmt->fetchAll();
+            if($registros){
+            $objetos = array();
+            foreach($registros as $registro){
+                $temporario = new Produto();
+                $temporario->setId($registro['id']);
+                $temporario->setCategoria_id($registro['categoria_id']);
+                $temporario->setNome($registro['nome']);
+                $temporario->setPreco($registro['preco']);
+                $temporario->setQuantidade($registro['quantidade']);
+                $objetos[] = $temporario;
+            }
+            return $objetos;
+        }
+        return false;
+        }        
         public function atualizar(){} 
         public function excluir(){} 
     }
